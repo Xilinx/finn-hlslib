f8252072b20473ce0c0cffcba72e8e598b68303d
Giulio <giuliog@xilinx.com>
2020-03-23 10:48:11 +0000

    Fixed bug in stream mvau

--- mvau.hpp
+++ mvau.hpp
@@ -205,107 +205,6 @@ void Matrix_Vector_Activate_Batch(hls::stream<TI> &in,
  * \param reps        Number of time the function has to be repeatedly executed (e.g. number of images)
  * \param r           Resource type for the hardware implementation of the MAC block
  */
- /*
-template<
-  unsigned MatrixW, unsigned MatrixH, unsigned SIMD, unsigned PE, 
-  typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity, typename TW,
-  typename TI, typename TO, typename TA, typename R
->
-void Matrix_Vector_Activate_Stream_Batch(hls::stream<TI> &in,
-				  hls::stream<TO> &out,
-          hls::stream<ap_uint<PE*TW::width>> &weight,
-				  TA  const &activation,
-				  int const  reps,
-				  R const &r) {
-
-  // how many different rows each neuron will compute
-  // alternatively: number of vertical matrix chunks
-  unsigned const  NF = MatrixH / PE;
-
-  // how many synapse groups each row is split into
-  // alternatively: number of horizontal matrix chunks
-  unsigned const  SF = MatrixW / SIMD;
-
-  // input vector buffers
-  TI  inputBuf[SF];
-#pragma HLS ARRAY_PARTITION variable=inputBuf complete dim=1
-
-  decltype(activation.init(0,0))  accu[1][PE];
-#pragma HLS ARRAY_PARTITION variable=accu complete dim=0
-
-  unsigned  nf   = 0;
-  unsigned  sf   = 0;
-  unsigned  tile = 0; // invariant: tile = nf*SF + sf
-
-  // param stream
-  TW w[PE];
-  // everything merged into a common iteration space (one "big" loop instead
-  // of smaller nested loops) to get the pipelinening the way we want
-  unsigned const TOTAL_FOLD = NF * SF;
-  for(unsigned  i = 0; i < reps * TOTAL_FOLD; i++) {
-#pragma HLS PIPELINE II=1
-    TI  inElem;
-
-    if(nf == 0) {
-      // read input from stream
-      inElem = in.read();
-      // store in appropriate buffer for reuse
-      inputBuf[sf] = inElem;
-    }
-    else {
-      // reuse buffered input
-      inElem = inputBuf[sf];
-    }
-
-    // read from the parameter stream
-    ap_uint<PE * TW::width> W_packed = weight.read();
-    // mask the last (SIMD-wide) bits of the streamed word
-    for (unsigned pe = 0; pe < PE; pe++) {
-      // W[pe] = streamed_word & 2^SIMD - 1 (little endian)
-      w[PE - pe - 1] = (TW)W_packed & (((ap_uint<TW::width + 1>)1 << (TW::width)) - 1);
-      W_packed >>= TW::width;
-    }
-
-    // Threshold Initialisation
-    if(sf == 0) {
-      for(unsigned pe = 0; pe < PE; pe++) {
-#pragma HLS UNROLL
-	    accu[0][pe] = activation.init(nf, pe);
-      }
-    }
-
-    // compute matrix-vector product for each processing element
-    for(unsigned  pe = 0; pe < PE; pe++) {
-#pragma HLS UNROLL
-      auto const  act = TSrcI()(inElem, 0);
-      auto const  wgt = TWeightI()(w[pe]);
-      //std::cout << " " << wgt << std::endl;
-      accu[0][pe] = mac<SIMD>(accu[0][pe], wgt, act, r, 0);
-    }
-
-    // keep track of which folded synapse/neuron we are processing
-    ++tile;
-    if(++sf == SF) {
-      // produce output and clear accumulators
-      auto  outElem = TDstI().template operator()<TO>();
-      for (unsigned  pe = 0; pe < PE; pe++) {
-#pragma HLS UNROLL
-	    outElem(pe,0,1) = activation.activate(nf, pe, accu[0][pe]);
-        std::cout << (ap_int<16>)accu[0][pe] << std::endl;
-          }
-
-      out.write(outElem);
-
-      // next folded neuron or image
-      sf = 0;
-      if(++nf == NF) {
-	    nf   = 0;
-	    tile = 0;
-      }
-    }
-  }
-}
-*/
 template<
   unsigned MatrixW, unsigned MatrixH, unsigned SIMD, unsigned PE, 
   typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity, typename TW,
@@ -380,7 +279,8 @@ void Matrix_Vector_Activate_Stream_Batch(hls::stream<TI> &in,
     for(unsigned  pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
       auto const  act = TSrcI()(inElem, 0);
-      auto const  wgt = w[pe];
+      auto const  wgt = TWeightI()(w[pe]);
+      //auto const  wgt = w[pe];
       accu[0][pe] = mac<SIMD>(accu[0][pe], wgt, act, r, 0);
     }
 
