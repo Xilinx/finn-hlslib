8121820af9a742df79fc6233d7dde7e083c2af2a
Giulio <giuliog@xilinx.com>
2020-08-11 14:37:07 +0100

    Fixed typo

--- tb/conv_stream_top.cpp
+++ tb/conv_stream_top.cpp
@@ -74,6 +74,6 @@ void Testbench_conv(stream<ap_uint<IFM_Channels1*INPUT_PRECISION> > & in, stream
          static_cast<hls::stream<ap_uint<PE1*ACTIVATION_PRECISION>>&>  (mvOut),
          paramStreamOut, PassThroughActivation<ap_uint<16>>(), numReps* OFMDim1 * OFMDim1, ap_resource_dsp()    );
 	
-	StreamingDataWidthConverter_Batch<PE*ACTIVATION_PRECISION, OFM_Channels1*ACTIVATION_PRECISION, OFMDim1 * OFMDim1 * (OFM_Channels1 / PE1)>(mvOut, out, reps);
+	StreamingDataWidthConverter_Batch<PE1*ACTIVATION_PRECISION, OFM_Channels1*ACTIVATION_PRECISION, OFMDim1 * OFMDim1 * (OFM_Channels1 / PE1)>(mvOut, out, reps);
 
 }
