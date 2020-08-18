3b9c641aee5770d40e306f8aef04828f3fc04f44
anonymous anonymous <ieeefccm2019@gmail.com>
2020-08-18 08:39:12 -0700

    fixed W1 type

--- tb/conv3_tb.cpp
+++ tb/conv3_tb.cpp
@@ -128,7 +128,7 @@ int main()
 			}
 		}
 	}
-	static	ap_fixed<WIDTH, _I> W1[OFM_Channels1][KERNEL_DIM][KERNEL_DIM][IFM_Channels1];
+	static	ap_uint<WIDTH> W1[OFM_Channels1][KERNEL_DIM][KERNEL_DIM][IFM_Channels1];
 	// initialize the weights
 	constexpr int TX = (IFM_Channels1*KERNEL_DIM*KERNEL_DIM) / SIMD1;
 	constexpr int TY = OFM_Channels1 / PE1;
