bf0fae572995cb8ec8568d7ce3de5e406462f66b
mmrahorovic <mmrahorovic@hotmail.com>
2022-01-24 15:23:54 +0000

    Minor updates to code style

--- slidingwindow.h
+++ slidingwindow.h
@@ -1856,19 +1856,20 @@ void ConvolutionInputGenerator_1D_lowbuffer(
 	R const &r
 ) {
 	static_assert((IFMChannels % SIMD) == 0, "SIMD parallelism must divide the number of IFM channels");
-	static_assert(OFMDim_x == ((IFMDim_x-ConvKernelDim_x)/Stride_x+1), "Unexpected OFM dimension");
+	static_assert(OFMDim_x == ((IFMDim_x - ConvKernelDim_x) / Stride_x + 1), "Unexpected OFM dimension");
 
 	constexpr unsigned  SIMD_COUNT  = IFMChannels / SIMD;
-	constexpr unsigned  BUFFER_SIZE = (ConvKernelDim_x-1) * SIMD_COUNT;
+	constexpr unsigned  BUFFER_SIZE = (ConvKernelDim_x - 1) * SIMD_COUNT;
 	constexpr unsigned  OUTPUT_SIZE = OFMDim_x * ConvKernelDim_x * SIMD_COUNT;
-	constexpr unsigned  INPUT_SIZE = IFMDim_x*SIMD_COUNT;
-	constexpr unsigned  WINDOW_SIZE = ConvKernelDim_x*SIMD_COUNT;
+	constexpr unsigned  INPUT_SIZE = IFMDim_x * SIMD_COUNT;
+	constexpr unsigned  WINDOW_SIZE = ConvKernelDim_x * SIMD_COUNT;
+	constexpr unsigned  OCNT_INITIAL = BUFFER_SIZE + (Stride_x - 1);
 
 	ap_uint<SIMD*Input_precision>  buffer[BUFFER_SIZE];
 	memory_resource(buffer, r);
 
 	for(unsigned  count_image = 0; count_image < numReps; count_image++) {
-		unsigned  ocnt = (ConvKernelDim_x-1)*SIMD_COUNT+(Stride_x-1);
+		signed  ocnt = OCNT_INITIAL < WINDOW_SIZE ? OCNT_INITIAL : -1;
 		unsigned  wp = 0;
 		unsigned  rp = 0;
 		unsigned  offset = 0;
@@ -1876,21 +1877,21 @@ void ConvolutionInputGenerator_1D_lowbuffer(
 		for(unsigned  i = 0; i < 1+OUTPUT_SIZE; i++) {
 #pragma HLS PIPELINE II=1
 			bool const  re = i > 0;
-			bool const  we = (i < WINDOW_SIZE) || (ocnt < SIMD_COUNT*Stride_x);
+			bool const  we = (i < WINDOW_SIZE) || (ocnt < SIMD_COUNT * Stride_x);
 			if(re) {
 				out.write(buffer[rp]);
 				if(++offset == WINDOW_SIZE){
 					offset = 0;
-					rp += 1+SIMD_COUNT*(Stride_x-1);
+					rp += 1 + SIMD_COUNT * (Stride_x - 1);
 					if(rp >= BUFFER_SIZE)  rp -= BUFFER_SIZE;
 				}
-				else{
+				else{ // Explicit else-block required to work around bug in RTL simulation
 					if(++rp >= BUFFER_SIZE)  rp -= BUFFER_SIZE;
 				}
-				if(++ocnt >= WINDOW_SIZE)  ocnt = 0;
+				if(++ocnt == WINDOW_SIZE)  ocnt = 0;
 			}
 			if(we) {
-				if (++inp_count<=INPUT_SIZE){
+				if (++inp_count <= INPUT_SIZE){
 					buffer[wp] = in.read();
 					if(++wp == BUFFER_SIZE)  wp = 0;
 				}
