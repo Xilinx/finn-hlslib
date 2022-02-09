f5ccd0e16fa329304fb0a5dda9fc22c684876976
mmrahorovic <mmrahorovic@hotmail.com>
2022-02-09 17:27:10 +0000

    [maxpool]: support for PE parallelism for 1D MaxPool layer

--- tb/data/pool_config.h
+++ tb/data/pool_config.h
@@ -29,10 +29,11 @@
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  ******************************************************************************/
-#define KERNEL_DIM 2
-#define WIDTH 4 
-#define FM_Channels1 16
-#define IFMDim1 16
-#define OFMDim1 8
-#define PRECISION 3
+
+#define KERNEL_DIM 5
+#define FM_Channels1 10
+#define IFMDim1 42
+#define OFMDim1 9
+#define PRECISION 16
+#define PE1 5
 
