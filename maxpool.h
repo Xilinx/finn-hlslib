8be64c8ad9e235ff798243361fd2a6f7a6ff4160
mmrahorovic <mmrahorovic@hotmail.com>
2022-02-20 22:55:41 +0000

    [maxpool]: support for ceil mode

--- maxpool.h
+++ maxpool.h
@@ -231,6 +231,7 @@ void StreamingMaxPool_Precision_Batch(stream<ap_uint<InStreamW> > & in,
  * \tparam PoolDim      Dimension of the Max Pool kernel
  * \tparam NumChannels  Number of Input Feature Maps
  * \tparam PE           Number of input rows (channels) computed in parallel
+ * \tparam OutputSize   Length of the Output Feature Map
  * \tparam ActType      DataType of the input activation (as used in the comparison)
  * \tparam min_value    Minimum value possible with the given ActType, used to initialize the value before the comparison
  * 
@@ -240,13 +241,12 @@ void StreamingMaxPool_Precision_Batch(stream<ap_uint<InStreamW> > & in,
  */
 
 template<unsigned int ImgDim, unsigned int PoolDim, unsigned int NumChannels, unsigned int PE,
-        typename ActType, int min_value
+        unsigned int OutputSize, typename ActType, int min_value
         >
 void StreamingMaxPool_Precision_1d(stream<ap_uint<PE*ActType::width> > & in,
         stream<ap_uint<PE*ActType::width> > & out) {
   CASSERT_DATAFLOW(NumChannels % PE == 0);
   constexpr unsigned PE_COUNT = NumChannels / PE;
-  constexpr unsigned OUTPUT_SIZE = ImgDim % PoolDim == 0 ? ImgDim / PoolDim : ImgDim / PoolDim + 1;
   // need buffer space for a single maxpooled pixel of the image
   ActType buf[PE_COUNT][PE];
 #pragma HLS ARRAY_PARTITION variable=buf complete dim=2
@@ -261,7 +261,7 @@ void StreamingMaxPool_Precision_1d(stream<ap_uint<PE*ActType::width> > & in,
 
   ap_uint<PE*ActType::width> inputData,outputData;
   unsigned input_count = 0;
-  for (unsigned int xp = 0; xp < OUTPUT_SIZE; xp++) {
+  for (unsigned int xp = 0; xp < OutputSize; xp++) {
     // Change to comparator
     for (unsigned int kx = 0; kx < PoolDim; kx++) {
       if (input_count++ < ImgDim){
