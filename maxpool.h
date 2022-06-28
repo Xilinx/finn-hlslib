882fd7c2311c1acc3806e818bb710a810b6897b9
Thomas B. Preußer <thomas.preusser@xilinx.com>
2022-06-28 08:54:55 +0100

    Rolling back additional AMD copyright.

--- maxpool.h
+++ maxpool.h
@@ -1,6 +1,5 @@
 /******************************************************************************
  *  Copyright (c) 2019, Xilinx, Inc.
- *  Copyright (c) 2022, Advanced Micro Devices, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
--- tb/data/gen_weigths.py
+++ tb/data/gen_weigths.py
@@ -1,5 +1,4 @@
 #   Copyright (c) 2019, Xilinx, Inc.
-#   Copyright (c) 2022, Advanced Micro Devices, Inc.
 #   All rights reserved.
 # 
 #   Redistribution and use in source and binary forms, with or without 
