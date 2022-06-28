7dd38fda9a8a8225e456f5c9ea3526a2936e3f08
Thomas B. Preußer <thomas.preusser@xilinx.com>
2022-06-28 07:53:43 +0100

    Code cleanup.

--- vvau.hpp
+++ vvau.hpp
@@ -53,7 +53,7 @@
 /**
  * \brief Vector vector activate function
  *
- * The function performs the multiplication between a weigth vector and the input activation vector,
+ * The function performs the multiplication between a weight vector and the input activation vector,
  * accumulating the results and then applying an activation function on the accumulated result.
  * It is used to implement depth-wise separable convolution
  * 
@@ -79,79 +79,81 @@
  * \param r           Resource type for the hardware implementation of the MAC block
  */
 template<
-  unsigned Channels, unsigned Kernel_2, unsigned SIMD, unsigned PE, unsigned MMV, 
-  typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity,
-  typename TI, typename TO, typename TW, typename TA, typename R
+	unsigned Channels, unsigned Kernel_2, unsigned SIMD, unsigned PE, unsigned MMV,
+	typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity,
+	typename TI, typename TO, typename TW, typename TA, typename R
 >
-void Vector_Vector_Activate_Batch(hls::stream<TI> &in,
-				  hls::stream<TO> &out,
-				  TW  const &weights,
-				  TA  const &activation,
-				  int const  reps,
-				  R const &r) {
+void Vector_Vector_Activate_Batch(
+	hls::stream<TI> &in,
+	hls::stream<TO> &out,
+	TW  const &weights,
+	TA  const &activation,
+	int const  reps,
+	R const &r
+) {
 
-  // how many different rows each neuron will compute
-  // alternatively: number of vertical matrix chunks
-  unsigned const  NF = Channels / PE;
+	// how many different rows each neuron will compute
+	// alternatively: number of vertical matrix chunks
+	constexpr unsigned  NF = Channels / PE;
 
-  // how many synapse groups each row is split into
-  // alternatively: number of horizontal matrix chunks
-  unsigned const  SF = (Channels*Kernel_2) / Channels;
-  decltype(activation.init(0,0))  accu[MMV][PE];
+	// how many synapse groups each row is split into
+	// alternatively: number of horizontal matrix chunks
+	constexpr unsigned  SF = (Channels*Kernel_2) / Channels;
+	decltype(activation.init(0,0))  accu[MMV][PE];
 #pragma HLS ARRAY_PARTITION variable=accu complete dim=0
 
-  unsigned  nf   = 0;
-  unsigned  sf   = 0;
-  unsigned  tile = 0; // invariant: tile = nf*SF + sf
-  // everything merged into a common iteration space (one "big" loop instead
-  // of smaller nested loops) to get the pipelinening the way we want
-  unsigned const TOTAL_FOLD = NF * SF ;//* Channels/SIMD;
-  for(unsigned  i = 0; i < reps * TOTAL_FOLD; i++) {
+	unsigned  nf   = 0;
+	unsigned  sf   = 0;
+	unsigned  tile = 0; // invariant: tile = nf*SF + sf
+	// everything merged into a common iteration space (one "big" loop instead
+	// of smaller nested loops) to get the pipelinening the way we want
+	constexpr unsigned  TOTAL_FOLD = NF * SF ;//* Channels/SIMD;
+	for(unsigned  i = 0; i < reps * TOTAL_FOLD; i++) {
 #pragma HLS pipeline style=flp II=1
-    TI  inElem;
-    inElem = in.read();
-    // Threshold Initialisation
-    if(sf == 0) {
-      for(unsigned  pe = 0; pe < PE; pe++) {
-        for(unsigned mmv = 0; mmv < MMV; mmv++) {
+		TI  inElem;
+		inElem = in.read();
+		// Threshold Initialisation
+		if(sf == 0) {
+			for(unsigned  pe = 0; pe < PE; pe++) {
+				for(unsigned mmv = 0; mmv < MMV; mmv++) {
 #pragma HLS UNROLL
-          accu[mmv][pe] = activation.init(nf, pe);
-        }
-      }
-    }
+					accu[mmv][pe] = activation.init(nf, pe);
+				}
+			}
+		}
 
-    // compute matrix-vector product for each processing element
-    auto const &w = weights.weights(tile);
-    for(unsigned  pe = 0; pe < PE; pe++) {
+		// compute matrix-vector product for each processing element
+		auto const &w = weights.weights(tile);
+		for(unsigned  pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
-      auto const  wgt = TWeightI()(w[pe]);
-      for (unsigned mmv = 0; mmv < MMV; mmv++){
-        auto const  act = TSrcI()(inElem, mmv);
-		accu[mmv][pe] += mul(wgt[0], act(pe,mmv), r);
-      }
-    }
+			auto const  wgt = TWeightI()(w[pe]);
+			for(unsigned mmv = 0; mmv < MMV; mmv++) {
+				auto const  act = TSrcI()(inElem, mmv);
+				accu[mmv][pe] += mul(wgt[0], act(pe,mmv), r);
+			}
+		}
 
-    // keep track of which folded synapse/neuron we are processing
-    ++tile;
-    if(++sf == SF) {
-      // produce output and clear accumulators
-      auto  outElem = TDstI().template operator()<TO>();
-      for (unsigned  pe = 0; pe < PE; pe++) {
+		// keep track of which folded synapse/neuron we are processing
+		++tile;
+		if(++sf == SF) {
+			// produce output and clear accumulators
+			auto  outElem = TDstI().template operator()<TO>();
+			for(unsigned  pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
-        for (unsigned mmv = 0; mmv < MMV; mmv++){
+				for(unsigned mmv = 0; mmv < MMV; mmv++) {
 #pragma HLS UNROLL
-          outElem(pe,mmv,1) = activation.activate(nf, pe, accu[mmv][pe]);
-        }
-      }
-      out.write(outElem);
-      // next folded neuron or image
-      sf = 0;
-      if(++nf == NF) {
-	    nf   = 0;
-	    tile = 0;
-      }
-    }
-  }
+					outElem(pe,mmv,1) = activation.activate(nf, pe, accu[mmv][pe]);
+				}
+			}
+			out.write(outElem);
+			// next folded neuron or image
+			sf = 0;
+			if(++nf == NF) {
+				nf   = 0;
+				tile = 0;
+			}
+		}
+	}
 }
 
 
@@ -184,87 +186,88 @@ void Vector_Vector_Activate_Batch(hls::stream<TI> &in,
  * \param r           Resource type for the hardware implementation of the MAC block
  */
 template<
-  unsigned Channels, unsigned Kernel_2, unsigned SIMD, unsigned PE, unsigned MMV, 
-  typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity, typename TW, 
-  typename TI, typename TO, typename TA, typename R
+	unsigned Channels, unsigned Kernel_2, unsigned SIMD, unsigned PE, unsigned MMV,
+	typename TSrcI = Identity, typename TDstI = Identity, typename TWeightI = Identity, typename TW,
+	typename TI, typename TO, typename TA, typename R
 >
-void Vector_Vector_Activate_Stream_Batch(hls::stream<TI> &in,
-				  hls::stream<TO> &out,
-				  hls::stream<ap_uint<PE*SIMD*TW::width>> &weights,
-				  TA  const &activation,
-				  int const  reps,
-				  R const &r) {
+void Vector_Vector_Activate_Stream_Batch(
+	hls::stream<TI> &in,
+	hls::stream<TO> &out,
+	hls::stream<ap_uint<PE*SIMD*TW::width>> &weights,
+	TA  const &activation,
+	int const  reps,
+	R const &r
+) {
 
-  // how many different rows each neuron will compute
-  // alternatively: number of vertical matrix chunks
-  unsigned const  NF = Channels / PE;
+	// how many different rows each neuron will compute
+	// alternatively: number of vertical matrix chunks
+	constexpr unsigned  NF = Channels / PE;
 
-  // how many synapse groups each row is split into
-  // alternatively: number of horizontal matrix chunks
-  unsigned const  SF = (Channels*Kernel_2) / Channels;
-  decltype(activation.init(0,0))  accu[MMV][PE];
+	// how many synapse groups each row is split into
+	// alternatively: number of horizontal matrix chunks
+	constexpr unsigned  SF = (Channels*Kernel_2) / Channels;
+	decltype(activation.init(0,0))  accu[MMV][PE];
 #pragma HLS ARRAY_PARTITION variable=accu complete dim=0
 
-  // unpacked and packed buffers for weight stream
-  Weights_Tile<SIMD, TW, PE > w;
-#pragma HLS ARRAY_PARTITION variable=w.m_weights complete dim=0
-  ap_uint<PE * SIMD * TW::width> W_packed;
-  unsigned  nf   = 0;
-  unsigned  sf   = 0;
-  unsigned  tile = 0; // invariant: tile = nf*SF + sf
-  // everything merged into a common iteration space (one "big" loop instead
-  // of smaller nested loops) to get the pipelinening the way we want
-  unsigned const TOTAL_FOLD = NF * SF ;//* Channels/SIMD;
-  for(unsigned  i = 0; i < reps * TOTAL_FOLD; i++) {
+	// unpacked and packed buffers for weight stream
+	unsigned  nf   = 0;
+	unsigned  sf   = 0;
+	unsigned  tile = 0; // invariant: tile = nf*SF + sf
+	// everything merged into a common iteration space (one "big" loop instead
+	// of smaller nested loops) to get the pipelinening the way we want
+	constexpr unsigned  TOTAL_FOLD = NF * SF ;//* Channels/SIMD;
+	for(unsigned  i = 0; i < reps * TOTAL_FOLD; i++) {
 #pragma HLS pipeline style=flp II=1
-    TI  inElem;
-    inElem = in.read();
-    // Threshold Initialisation
-    if(sf == 0) {
-      for(unsigned  pe = 0; pe < PE; pe++) {
-        for(unsigned mmv = 0; mmv < MMV; mmv++) {
+		TI  inElem;
+		inElem = in.read();
+		// Threshold Initialisation
+		if(sf == 0) {
+			for(unsigned  pe = 0; pe < PE; pe++) {
+				for(unsigned mmv = 0; mmv < MMV; mmv++) {
 #pragma HLS UNROLL
-          accu[mmv][pe] = activation.init(nf, pe);
-        }
-      }
-    }
+					accu[mmv][pe] = activation.init(nf, pe);
+				}
+			}
+		}
 
-    // read from the parameter stream
-    W_packed = weights.read();
-    for (unsigned pe = 0; pe < PE; pe++) {
+		// Packed and unpacked weight representations
+		ap_uint<PE * SIMD * TW::width> const  W_packed = weights.read();
+		Weights_Tile<SIMD, TW, PE>  w;
+#pragma HLS ARRAY_PARTITION variable=w.m_weights complete dim=0
+		for(unsigned pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
-      w.m_weights[pe] = W_packed((pe+1)*SIMD*TW::width-1,pe*SIMD*TW::width);
-    }
+			w.m_weights[pe] = W_packed((pe+1)*SIMD*TW::width-1, pe*SIMD*TW::width);
+		}
 
-    for(unsigned  pe = 0; pe < PE; pe++) {
+		for(unsigned  pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
-      auto const  wgt = TWeightI()(w[pe]);
-      for (unsigned mmv = 0; mmv < MMV; mmv++){
-        auto const  act = TSrcI()(inElem, mmv);
-		accu[mmv][pe] += mul(wgt[0], act(pe,mmv), r);
-      }
-    }
+			auto const  wgt = TWeightI()(w[pe]);
+			for(unsigned mmv = 0; mmv < MMV; mmv++) {
+				auto const  act = TSrcI()(inElem, mmv);
+				accu[mmv][pe] += mul(wgt[0], act(pe,mmv), r);
+			}
+		}
 
-    // keep track of which folded synapse/neuron we are processing
-    ++tile;
-    if(++sf == SF) {
-      // produce output and clear accumulators
-      auto  outElem = TDstI().template operator()<TO>();
-      for (unsigned  pe = 0; pe < PE; pe++) {
+		// keep track of which folded synapse/neuron we are processing
+		++tile;
+		if(++sf == SF) {
+			// produce output and clear accumulators
+			auto  outElem = TDstI().template operator()<TO>();
+			for(unsigned  pe = 0; pe < PE; pe++) {
 #pragma HLS UNROLL
-        for (unsigned mmv = 0; mmv < MMV; mmv++){
+				for(unsigned mmv = 0; mmv < MMV; mmv++) {
 #pragma HLS UNROLL
-          outElem(pe,mmv,1) = activation.activate(nf, pe, accu[mmv][pe]);
-        }
-      }
-      out.write(outElem);
-      // next folded neuron or image
-      sf = 0;
-      if(++nf == NF) {
-	    nf   = 0;
-	    tile = 0;
-      }
-    }
-  }
+					outElem(pe,mmv,1) = activation.activate(nf, pe, accu[mmv][pe]);
+				}
+			}
+			out.write(outElem);
+			// next folded neuron or image
+			sf = 0;
+			if(++nf == NF) {
+				nf   = 0;
+				tile = 0;
+			}
+		}
+	}
 }
 #endif
