e9946e5e56acd85837e8e79224d2bb60764bed69
auphelia <jakobapk@web.de>
2022-07-06 09:53:27 +0100

    [Maxpool] Cast comparator to AccType

--- maxpool.h
+++ maxpool.h
@@ -426,7 +426,7 @@ void AccPool_Batch(hls::stream<ap_uint<PECount * ActType::width> > & in,
           // Threshold and assign to right bits of output buffers
           ActType const  val = thin((pe+1) * ActType::width - 1,pe * ActType::width);
           AccType const  acc = accbank((pe+1) * AccType::width - 1,pe * AccType::width);
-          AccType const  result = val + (pixel == 0? 0 : acc);
+          AccType const  result = val + (pixel == 0? AccType(0) : acc);
           accbank((pe+1) * AccType::width - 1,pe * AccType::width) = result;
         }
         accumulators[fold] = accbank;     
