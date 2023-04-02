843aeb5aa19de3e16eb98f11d46f966eee9343ea
icolbert <Ian.Colbert@amd.com>
2023-04-01 17:12:21 -0700

    Updating fm pixel padding top-level function

--- tb/data/config_fmpp.h
+++ tb/data/config_fmpp.h
@@ -27,26 +27,15 @@
  *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
  *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  ******************************************************************************/
-#include <hls_stream.h>
-using namespace hls;
-#include "ap_int.h"
-#include "../bnn-library.h"
-#include "data/config_fmpp.h"
 
-void test_fm_pixel_padding(
-	stream<ap_uint<SIMD1*INPUT_WIDTH>> &src,
-	stream<ap_uint<SIMD1*INPUT_WIDTH>> &dst
-) {
-#pragma HLS DATAFLOW
-	FMPadding_Pixel_Nonsquare<
-		OUTPUT_DIM_X,
-		OUTPUT_DIM_Y,
-		XSTRIDE,
-		YSTRIDE,
-		CHANNELS,
-		SIMD1,
-		ap_uint<INPUT_WIDTH>
-	>(src, dst);
-}
+constexpr unsigned  SIMD1 = 1;
+constexpr unsigned  INPUT_WIDTH = 8;
+constexpr unsigned  INPUT_DIM_X = 30;
+constexpr unsigned  INPUT_DIM_Y = 40;
+constexpr unsigned  CHANNELS = 3;
+constexpr unsigned  XSTRIDE = 5;
+constexpr unsigned  YSTRIDE = 3;
+
+constexpr unsigned  OUTPUT_DIM_X = INPUT_DIM_X + (INPUT_DIM_X - 1) * (XSTRIDE - 1);
+constexpr unsigned  OUTPUT_DIM_Y = INPUT_DIM_Y + (INPUT_DIM_Y - 1) * (YSTRIDE - 1);
