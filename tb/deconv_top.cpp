661fcd058c9633f8a3fe3fa4b983d4672a2281e2
icolbert <Ian.Colbert@amd.com>
2023-04-02 21:28:34 -0700

    Adding fmpadding and convlayer batch

--- tb/deconv_top.cpp
+++ tb/deconv_top.cpp
@@ -35,46 +35,37 @@ using namespace hls;
 
 #include "../bnn-library.h"
 #include "../weights.hpp"
-// #include "activations.hpp"
+#include "../activations.hpp"
 // #include "interpret.hpp"
 // #include "mvau.hpp"
-// #include "conv.hpp"
 #include "data/memdata_deconv2d.h"
 #include "data/config_deconv2d.h"
 
 void test_deconv2d(
-	stream<ap_uint<DeconvIFMCh*IPrecision> > &src,
-	stream<ap_uint<DeconvOFMCh*OPrecision> > &dst
+	stream<ap_uint<DeconvIFMCh*IPrecision> > & src,
+	stream<ap_uint<DeconvOFMCh*OPrecision> > & dst
 ){
 #pragma HLS DATAFLOW
-
-	ap_uint<DeconvIFMCh*IPrecision>  val = 0;
-	val = src.read();
-
-	// stream<ap_uint<IFM_Channels1*INPUT_PRECISION> > conv_input("input_stream");
-	// FMPadding_Pixel_Nonsquare<
-	// 	OUTPUT_DIM_X, // dimension expected by conv
-	// 	OUTPUT_DIM_Y, // dimension expected by conv
-	// 	XSTRIDE, // stride along pixel padding
-	// 	YSTRIDE, // stride along pixel padding
-	// 	CHANNELS, // num channels expected by conv (input channels)
-	// 	SIMD1, // packing along the channel dim
-	// 	ap_uint<INPUT_WIDTH> // data type of values
-	// >(src, conv_input);
-
-	// // TODO - replace weights and pass-through activation
-	// // TODO - figure out why pass-through activation is 16 bits
-	// // TODO - figure out why we are using DSP
-	// ConvLayer_Batch<
-	// 	KERNEL_DIM,
-	// 	IFM_Channels1,
-	// 	IFMDim1,
-	// 	OFM_Channels1,
-	// 	OFMDim1,
-	// 	SIMD1,
-	// 	PE1,
-	// 	Slice<ap_uint<INPUT_PRECISION> >,
-	// 	Slice<ap_uint<ACTIVATION_PRECISION> >,
-	// 	Identity
-	// >(conv_inputs, dst, PARAM::weights, PassThroughActivation<ap_uint<16>>(), 1, ap_resource_dsp());
+	stream<ap_uint<DeconvIFMCh*IPrecision> > conv_input("input_stream");
+	FMPadding_Pixel_Nonsquare<
+		FMPadODim, // dimension expected by conv
+		FMPadODim, // dimension expected by conv
+		FMPadStride, // stride along pixel padding
+		FMPadStride, // stride along pixel padding
+		DeconvIFMCh, // num channels expected by conv (input channels)
+		ConvSIMD1, // packing along the channel dim
+		ap_uint<IPrecision> // data type of values
+	>(src, conv_input);
+	ConvLayer_Batch<
+		DeconvKernel, // conv and deconv have same kernel size
+		DeconvIFMCh,
+		FMPadODim, // output of fm padding is input of conv
+		DeconvOFMCh,
+		DeconvOFDim,
+		ConvSIMD1,
+		ConvPE1,
+		Slice<ap_uint<IPrecision> >,
+		Slice<ap_uint<OPrecision> >,
+		Identity
+	>(conv_input, dst, PARAM::weights, PassThroughActivation<ap_uint<16>>(), 1, ap_resource_dsp());
 }
