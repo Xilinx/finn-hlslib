1a5be9ddcbd8cc7e364de24e3d09f1e4864341b4
icolbert <Ian.Colbert@amd.com>
2023-04-02 21:37:00 -0700

    Adding generated config/memdata

---
+++ tb/data/config_deconv2d.h
@@ -0,0 +1,14 @@
+constexpr unsigned  DeconvIFDim = 4;
+constexpr unsigned  DeconvIFMCh = 3;
+constexpr unsigned  DeconvOFDim = 8;
+constexpr unsigned  DeconvOFMCh = 4;
+constexpr unsigned  DeconvKernel = 4;
+constexpr unsigned  DeconvStride = 2;
+constexpr unsigned  DeconvPadding = 1;
+constexpr unsigned  IPrecision = 4;
+constexpr unsigned  OPrecision = 16;
+constexpr unsigned  WPrecision = 4;
+constexpr unsigned  ConvSIMD1 = 3;
+constexpr unsigned  ConvPE1 = 2;
+constexpr unsigned  FMPadODim = 7;
+constexpr unsigned  FMPadStride = 2;
