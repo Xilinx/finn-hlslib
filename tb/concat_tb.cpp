376a35b225f069e014a2ec6f4b9fb2af6dd3293e
mdaniowi <mdaniowi@amd.com>
2024-09-17 13:44:57 +0100

    headers and cleanup

--- tb/concat_tb.cpp
+++ tb/concat_tb.cpp
@@ -1,3 +1,45 @@
+/******************************************************************************
+ *  Copyright (c) 2024, Advanced Micro Devices, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1.  Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2.  Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *  3.  Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION). HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+/*******************************************************************************
+ *
+ *  Authors: Michal Danilowicz <danilowi@agh.edu.pl>     
+ *
+ *  \file concat_tb.cpp
+ *
+ *  Testbench for the channel concatenation operation.
+ *
+ *******************************************************************************/
+
 #include <iostream>
 
 #include <hls_stream.h>
@@ -19,7 +61,7 @@ int main()
 	hls::stream<OUT_TYPE> out_V ("out_V");
 	hls::stream<OUT_TYPE> expected ("expected");
 
-	for (unsigned int counter = 0; counter < NUM_VECTORS; counter++){
+	for (unsigned int counter = 0; counter < REP_COUNT; counter++){
 		for(unsigned int f = 0; f < NUM_FOLDS0; f++){
 			IN_TYPE0 el0 = (EL_TYPE0)1;
 			in0_V.write(el0);
@@ -60,5 +102,3 @@ int main()
 		}
 	}
 }
-
-
