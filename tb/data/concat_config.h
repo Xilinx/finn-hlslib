c3820208f946efd9ed35423bd958bfbfa3e9cade
mdaniowi <mdaniowi@amd.com>
2024-09-17 13:42:16 +0100

    split and concat configs cleanup

--- tb/data/concat_config.h
+++ tb/data/concat_config.h
@@ -1,22 +1,23 @@
-#define SIMD 2
+#include <ap_int.h>
+
 #define NUM_INPUTS 4
-
-#define EL_TYPE0 ap_uint<3>
-#define EL_TYPE1 ap_uint<3>
-#define EL_TYPE2 ap_uint<3>
-#define EL_TYPE3 ap_uint<3>
-#define EL_TYPE_OUT ap_uint<3>
-
-#define IN_TYPE0 hls::vector<EL_TYPE0, SIMD>
-#define IN_TYPE1 hls::vector<EL_TYPE1, SIMD>
-#define IN_TYPE2 hls::vector<EL_TYPE2, SIMD>
-#define IN_TYPE3 hls::vector<EL_TYPE3, SIMD>
-#define OUT_TYPE hls::vector<EL_TYPE_OUT, SIMD>
-
 #define NUM_FOLDS0 1
 #define NUM_FOLDS1 1
 #define NUM_FOLDS2 3
 #define NUM_FOLDS3 2
 #define TOTAL_FOLDS 7
+#define REP_COUNT 3
 
-#define NUM_VECTORS 3
+constexpr unsigned SIMD = 2;
+
+using EL_TYPE0 = ap_uint<3>;
+using EL_TYPE1 = ap_uint<3>;
+using EL_TYPE2 = ap_uint<3>;
+using EL_TYPE3 = ap_uint<3>;
+using EL_TYPE_OUT = ap_uint<3>;
+
+using IN_TYPE0 = hls::vector<EL_TYPE0, SIMD>;
+using IN_TYPE1 = hls::vector<EL_TYPE1, SIMD>;
+using IN_TYPE2 = hls::vector<EL_TYPE2, SIMD>;
+using IN_TYPE3 = hls::vector<EL_TYPE3, SIMD>;
+using OUT_TYPE = hls::vector<EL_TYPE_OUT, SIMD>;
--- tb/data/split_config.h
+++ tb/data/split_config.h
@@ -1,13 +1,13 @@
-#define SIMD 2
+#include <ap_int.h>
+
 #define NUM_OUTPUTS 3
-
-#define EL_TYPE ap_uint<3>
-
-#define IN_TYPE hls::vector<EL_TYPE, SIMD>
-
 #define NUM_FOLDS0 1
 #define NUM_FOLDS1 1
 #define NUM_FOLDS2 3
 #define FOLDS_PER_OUTPUT {NUM_FOLDS0, NUM_FOLDS1, NUM_FOLDS2}
+#define REP_COUNT 3
 
-#define NUM_VECTORS 3
+constexpr unsigned SIMD = 2;
+
+using EL_TYPE = ap_uint<3>;
+using IN_TYPE = hls::vector<EL_TYPE, SIMD>;
