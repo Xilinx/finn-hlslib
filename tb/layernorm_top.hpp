c08553efb8f27340f3cbb6eccd9a88cb84f0b421
leah <leah.sieder@gmx.de>
2025-12-18 09:57:46 +0000

    Add top and tb files for layernorm.

---
+++ tb/layernorm_top.hpp
@@ -0,0 +1,27 @@
+/****************************************************************************
+ * Copyright (C) 2025, Advanced Micro Devices, Inc.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @author	Shane T. Fleming <shane.fleming@amd.com>
+ * @author	Thomas B. Preußer <thomas.preusser@amd.com>
+ ****************************************************************************/
+#ifndef LAYERNORM_TOP_HPP
+#define LAYERNORM_TOP_HPP
+
+#include <hls_stream.h>
+#include <hls_vector.h>
+
+
+constexpr unsigned  N    = 384;
+constexpr unsigned  SIMD =  4;
+using  TI = float;
+using  TO = float;
+
+void layernorm_top(
+	hls::stream<hls::vector<TI, SIMD>> &src,
+	hls::stream<hls::vector<TO, SIMD>> &dst
+);
+
+#endif
