6b4034780ec846d717a8108f0259cbf78b419c1f
Thomas B. Preußer <thomas.preusser@xilinx.com>
2026-02-25 04:51:35 +0000

    Simplified to unconditional writing in pack expansion.

--- dup.hpp
+++ dup.hpp
@@ -18,32 +18,32 @@
 
 // Helper for parameter pack indexing.
 namespace {
-	/** Recursive selector for writing to stream with represented index. */
-	template<unsigned  IDX, typename... TO>
+	/** Recursive writing to all streams in pack. */
+	template<typename... TO>
 	class PackWriter {};
 
 	/** Terminating with last stream in stream pack. */
-	template<unsigned  IDX, typename  T0>
-	class PackWriter<IDX, T0> {
+	template<typename  T0>
+	class PackWriter<T0> {
 	public:
 		template<typename TI>
-		void write(unsigned const  idx, TI &x, hls::stream<T0>& dst0) {
+		void write(TI &x, hls::stream<T0>& dst0) {
 #pragma HLS inline
-			if(idx == IDX)  dst0.write(x);
+			dst0.write(x);
 		}
 	};
 
-	/** Matching own index or recursively passing further into the pack. */
-	template<unsigned  IDX, typename  T0, typename... TO>
-	class PackWriter<IDX, T0, TO...> {
-		PackWriter<IDX+1, TO...>  inner;
+	/** Write and pass recursively further into the pack. */
+	template<typename  T0, typename... TO>
+	class PackWriter<T0, TO...> {
+		PackWriter<TO...>  inner;
 
 	public:
 		template<typename TI>
-		void write(unsigned const  idx, TI &x, hls::stream<T0>& dst0, hls::stream<TO>&... dst) {
+		void write(TI &x, hls::stream<T0>& dst0, hls::stream<TO>&... dst) {
 #pragma HLS inline
-			if(idx != IDX)  return  inner.write(idx, x, dst...);
-			else  dst0.write(x);
+			dst0.write(x);
+			inner.write(x, dst...);
 		}
 	};
 }
@@ -58,16 +58,13 @@ void StreamingDup(
 	hls::stream<TO>&... dst
 ){
 	constexpr unsigned  N = sizeof...(TO);
-	static PackWriter<0, TO...>  writer;
+	static PackWriter<TO...>  writer;
 
 #pragma HLS pipeline II=1 style=flp
 
 	if(!src.empty()) {
 		TI const  x = src.read();
-		for(unsigned  i = 0; i < N; i++) {
-#pragma HLS unroll
-			writer.write(i, x, dst...);
-		}
+		writer.write(x, dst...);
 	}
 }
 
